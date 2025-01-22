755c5d3def924c9c748355fd0ac95b7f1e30410c
Nipuni Udeshika <nipuni24@stu.codepark.university>
2025-01-22 19:04:08 +0530

    load data from dim_branch table

---
+++ sql/load_data.sql
@@ -0,0 +1,3 @@
+COPY dim_branch FROM 's3://kegalle-sales-data/dim_branch.csv'
+CREDENTIALS 'aws_access_key_id=AKIAXEFUNKMCLMCWFI44;aws_secret_access_key=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'
+CSV IGNOREHEADER 1;
