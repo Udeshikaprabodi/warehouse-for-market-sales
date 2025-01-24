31dfd4ba2844b76bd167832938a1b4aab9a41174
Nipuni Udeshika <nipuni24@stu.codepark.university>
2025-01-24 09:37:21 +0530

    load environment variables and get credentials

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,3 +1,15 @@
 COPY dim_branch FROM 's3://kegalle-sales-data/dim_branch.csv'
 CREDENTIALS 'aws_access_key_id=AKIAXEFUNKMCLMCWFI44;aws_secret_access_key=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'
 CSV IGNOREHEADER 1;
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get credentials from environment variables
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+S3_BUCKET = "kegalle-sales-data"
+
