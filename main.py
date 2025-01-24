2a97ce062a568b42061af8a96457fc108c11703d
Nipuni Udeshika <nipuni24@stu.codepark.university>
2025-01-24 16:04:23 +0530

    Load environment variables for main.py

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import os
 import boto3
 import psycopg2
 from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+REDSHIFT_ENDPOINT = os.getenv("REDSHIFT_ENDPOINT")
+REDSHIFT_USER = os.getenv("REDSHIFT_USER")
+REDSHIFT_PASSWORD = os.getenv("REDSHIFT_PASSWORD")
+REDSHIFT_DB = os.getenv("REDSHIFT_DB")
+REDSHIFT_PORT = os.getenv("REDSHIFT_PORT")
+S3_BUCKET = os.getenv("S3_BUCKET")
