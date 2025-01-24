dbc72ac8314caf6070d3acb8a8a3fe5674788f29
Nipuni Udeshika <nipuni24@stu.codepark.university>
2025-01-24 16:38:43 +0530

    update load_data.sql file

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,45 +1,21 @@
 
 
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Get credentials from environment variables
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-S3_BUCKET = "kegalle-sales-data"
-
-# SQL COPY commands with environment variables
-COPY_DIM_BRANCH = f"""
-COPY dim_branch FROM 's3://{S3_BUCKET}/dim_branch.csv'
-CREDENTIALS 'aws_access_key_id={AWS_ACCESS_KEY};aws_secret_access_key={AWS_SECRET_KEY}'
+COPY dim_branch
+FROM 's3://kegalle-sales-data/dim_branch.csv'
+CREDENTIALS 'aws_access_key_id=<AWS_ACCESS_KEY>;aws_secret_access_key=<AWS_SECRET_KEY>'
 CSV IGNOREHEADER 1;
-"""
 
-COPY_DIM_DATE = f"""
-COPY dim_date FROM 's3://{S3_BUCKET}/dim_date.csv'
-CREDENTIALS 'aws_access_key_id={AWS_ACCESS_KEY};aws_secret_access_key={AWS_SECRET_KEY}'
+COPY dim_product
+FROM 's3://kegalle-sales-data/dim_product.csv'
+CREDENTIALS 'aws_access_key_id=<AWS_ACCESS_KEY>;aws_secret_access_key=<AWS_SECRET_KEY>'
 CSV IGNOREHEADER 1;
-"""
 
-COPY_DIM_PRODUCT = f"""
-COPY dim_product FROM 's3://{S3_BUCKET}/dim_product.csv'
-CREDENTIALS 'aws_access_key_id={AWS_ACCESS_KEY};aws_secret_access_key={AWS_SECRET_KEY}'
+COPY dim_date
+FROM 's3://kegalle-sales-data/dim_date.csv'
+CREDENTIALS 'aws_access_key_id=<AWS_ACCESS_KEY>;aws_secret_access_key=<AWS_SECRET_KEY>'
 CSV IGNOREHEADER 1;
-"""
 
-COPY_FACT_SALES = f"""
-COPY fact_sales FROM 's3://{S3_BUCKET}/fact_sales.csv'
-CREDENTIALS 'aws_access_key_id={AWS_ACCESS_KEY};aws_secret_access_key={AWS_SECRET_KEY}'
+COPY fact_sales
+FROM 's3://kegalle-sales-data/fact_sales.csv'
+CREDENTIALS 'aws_access_key_id=<AWS_ACCESS_KEY>;aws_secret_access_key=<AWS_SECRET_KEY>'
 CSV IGNOREHEADER 1;
-"""
-
-
-
-# Print the queries to verify
-print(COPY_DIM_BRANCH)
-print(COPY_DIM_DATE)
-print(COPY_DIM_PRODUCT)
-print(COPY_FACT_SALES)
