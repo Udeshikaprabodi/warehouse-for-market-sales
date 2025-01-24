e74bc4d01730c69bf8b99d0bbb7e608a34bc7c53
Nipuni Udeshika <nipuni24@stu.codepark.university>
2025-01-24 12:25:03 +0530

    load data from dim_branch table

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -11,3 +11,10 @@ AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 S3_BUCKET = "kegalle-sales-data"
 
+# SQL COPY commands with environment variables
+COPY_DIM_BRANCH = f"""
+COPY dim_branch FROM 's3://{S3_BUCKET}/dim_branch.csv'
+CREDENTIALS 'aws_access_key_id={AWS_ACCESS_KEY};aws_secret_access_key={AWS_SECRET_KEY}'
+CSV IGNOREHEADER 1;
+"""
+
