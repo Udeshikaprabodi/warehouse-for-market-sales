39305b4c09e699ca86c7218c826850c477924b08
Nipuni Udeshika <nipuni24@stu.codepark.university>
2025-01-24 09:38:59 +0530

    get credentials from environment variables

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,6 +1,4 @@
-COPY dim_branch FROM 's3://kegalle-sales-data/dim_branch.csv'
-CREDENTIALS 'aws_access_key_id=AKIAXEFUNKMCLMCWFI44;aws_secret_access_key=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'
-CSV IGNOREHEADER 1;
+
 
 import os
 from dotenv import load_dotenv
